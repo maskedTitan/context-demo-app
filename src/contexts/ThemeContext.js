85990948e8252f1eb95eea86018d15ddfd85a2d0
maskedTitan <harshamarri18@gmail.com>
2020-04-05 19:12:44 +0530

    replaced components from class based to functional components

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -1,26 +1,15 @@
-import React, { Component, createContext } from 'react';
+import React, { createContext } from 'react';
+import useToggleState from '../hooks/useToggleState';
 
 export const ThemeContext = createContext();
 
-export class ThemeProvider extends Component {
-	constructor(props) {
-		super(props);
-		this.state = { isDarkMode: false };
-		this.toggleTheme = this.toggleTheme.bind(this);
-	}
-	toggleTheme() {
-		this.setState({ isDarkMode: !this.state.isDarkMode });
-	}
-	render() {
-		return (
-			<ThemeContext.Provider value={{ ...this.state, toggleTheme: this.toggleTheme }}>
-				{this.props.children}
-			</ThemeContext.Provider>
-		);
-	}
+export function ThemeProvider(props) {
+	const [ isDarkmode, toggleTheme ] = useToggleState();
+
+	return <ThemeContext.Provider value={{ isDarkmode, toggleTheme }}>{props.children}</ThemeContext.Provider>;
 }
 
 //Higher Order Component
-export const withThemeContext = (Component) => (props) => {
-	return <ThemeContext.Consumer>{(value) => <Component themeContext={value} {...props} />}</ThemeContext.Consumer>;
-};
+// export const withThemeContext = (Component) => (props) => {
+// 	return <ThemeContext.Consumer>{(value) => <Component themeContext={value} {...props} />}</ThemeContext.Consumer>;
+// };
